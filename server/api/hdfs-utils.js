4703e1dcc0eebfa3a061c6520dbb230d3795c511
Jared Noble <jsnoble1@gmail.com>
2015-07-07 14:41:56 -0700

    added range functionality, and altered config

--- server/api/hdfs-utils.js
+++ server/api/hdfs-utils.js
@@ -1,20 +1,20 @@
 'use strict';
 var urlParser = require('url');
+var _ = require('lodash');
+var Promise = require('bluebird');
 
-exports.checkTicket = function(req, endpoint){
+function checkTicket(req, endpoint) {
     var endpointConfig = endpoint[req.params.id];
 
     return req.query.ticket === endpointConfig.ticket
-};
+}
 
-exports.validateQuery = function (req, endpoint) {
-
+function validateQuery(req, endpoint) {
     var isValid = true;
     var error = [];
     var parsedUrl = urlParser.parse(req.url);
     var path = parsedUrl.pathname;
     var endpointConfig = endpoint[req.params.id];
-
 
     if (!endpointConfig) {
         isValid = false;
@@ -30,4 +30,67 @@ exports.validateQuery = function (req, endpoint) {
     }
 
     return {isValid: isValid, path: path, error: {error: error.join(' | ')}}
+}
+
+function formatConfig(obj) {
+    var clone = _.clone(obj);
+
+    if (!clone.path_prefix) {
+        clone.path_prefix = '/webhdfs/v1';
+    }
+    if (clone.directory) {
+        //checking if there is a '/' in-between the two
+        if (!(clone.path_prefix[clone.path_prefix.length - 1] === '/') && !(clone.directory[0] === '/')) {
+            clone.path_prefix += '/' + clone.directory;
+        }
+        else {
+            clone.path_prefix += clone.directory;
+        }
+    }
+
+    return clone;
+}
+
+function getData(client, queryPath, options) {
+    var options = options ? options : {};
+
+    return new Promise(function (resolve, reject) {
+        client.open(queryPath, options, function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data)
+            }
+        });
+    });
+}
+
+function getChunks(client, query, res, offset, length, total, statusCode) {
+    getData(client, query.path, {offset: offset, length: length})
+        .then(function (data) {
+            res.status(statusCode).write(data);
+            if (offset + length >= total) {
+                res.end();
+                return;
+            }
+            else {
+                var newOffset = offset + length;
+                var nextLength = newOffset + length <= total ? length : total - newOffset;
+                return getChunks(client, query, res, newOffset, nextLength, total)
+            }
+        });
+}
+
+function processError(err) {
+    return {error: err.message};
+}
+
+module.exports = {
+    checkTicket: checkTicket,
+    validateQuery: validateQuery,
+    formatConfig: formatConfig,
+    getData: getData,
+    getChunks: getChunks,
+    processError: processError
 };
--- server/api/hdfs.js
+++ server/api/hdfs.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 var hdfsClient = require('node-webhdfs').WebHDFSClient;
 var utils = require('./hdfs-utils');
 
@@ -8,6 +8,17 @@ module.exports = function (config) {
 
     return function (req, res) {
         var ticketIsValid = utils.checkTicket(req, endpoint);
+        var reqOptions = {};
+        var startByte = 0;
+        var statusCode = 200;
+        var isRange = false;
+
+        if (req.headers.range) {
+            startByte = req.headers.range;
+            statusCode = 206;
+            reqOptions.offset = req.headers.range;
+            isRange = true;
+        }
 
         if (!ticketIsValid) {
             res.status(401).json({error: 'invalid ticket for endpoint'})
@@ -19,15 +30,31 @@ module.exports = function (config) {
                 res.status(400).json(query.error)
             }
             else {
-                var endpointConfig = endpoint[req.params.id];
+                var endpointConfig = utils.formatConfig(endpoint[req.params.id]);
                 var client = new hdfsClient(endpointConfig);
+                //adjust byteInterval to change how big the slice is, set to 50 megabytes
+                var byteInterval = 50000000; //5,2,2.5
 
-                client.open(query.path, function (err, data) {
+                client.getFileStatus(query.path, function (err, bytes) {
                     if (err) {
-                        res.send(err);
+                        res.send(utils.processError(err));
+                    }  //if less then 50 megabytes just send file
+                    if (bytes.length < byteInterval) {
+
+                        utils.getData(client, query.path, reqOptions)
+                            .then(function (data) {
+                                res.status(statusCode).send(data);
+                            });
                     }
-                    res.send(data);
+                    else {
+                        var getChunks = utils.getChunks;
+
+                        getChunks(client, query, res, startByte, byteInterval, bytes.length, statusCode);
+
+                    }
+
                 });
+
             }
         }
     }
